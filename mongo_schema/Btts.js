cfdbf0e0b0742a6098795b37aa58966e3c5814fa
Alexey Krasnoshchekov <alexburnitdown@gmail.com>
2023-02-03 17:08:14 +0300

    first

--- mongo_schema/Btts.js
+++ mongo_schema/Btts.js
@@ -1,20 +1,24 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
-const bttsSchema = new Schema(
-  {
-    source: { type: String, required: true },
-    action: { type: String, required: true },
-    homeTeam: { type: String, required: true },
-    awayTeam: { type: String, required: true }
-  },
-  {
-    timestamps: true,
-  }
+// Btts model
+const Btts = mongoose.model(
+  'Btts',
+  new mongoose.Schema(
+    {
+      source: { type: String, required: true },
+      action: { type: String, required: true },
+      homeTeam: { type: String, required: true },
+      awayTeam: { type: String, required: false },
+      resultDescription: { type: String, required: false },
+      btts: { type: Boolean, required: false },
+      over25: { type: Boolean, required: false },
+    },
+    {
+      timestamps: true,
+    }
+  )
 );
-
-const Btts =
-  mongoose.models?.Btts || mongoose.model('Headline', bttsSchema);
 
 // export default Headline;
 module.exports = { Btts };
